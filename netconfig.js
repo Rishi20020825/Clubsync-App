d0ab6878e0fb5e04cb10d4941e7fc1a3bbf328d4
Rishi20020825 <rasheenmohommed25@gmail.com>
2025-07-17 16:08:25 +0530

    Login Error Solved

--- netconfig.js
+++ netconfig.js
@@ -1,3 +1,3 @@
 export const netconfig = {
-    API_BASE_URL: 'http://192.168.102.201:3000', // Change this when your network changes
+    API_BASE_URL: 'http://192.168.43.88:3000', // Change this when your network changes
 };
