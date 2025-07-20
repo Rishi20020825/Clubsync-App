d8c126a2cd5b79ecd77b7855e6d7074d0d3de7a5
kaveesha nirmal <kaveeshanp2002@gmail.com>
2025-07-21 02:27:27 +0530

    Update for emulator

--- netconfig.js
+++ netconfig.js
@@ -1,3 +1,4 @@
 export const netconfig = {
-    API_BASE_URL: 'http://192.168.102.201:3000', // Change this when your network changes
+    API_BASE_URL: 'http://10.0.2.2:3000', // Change this when your network changes
+    // API_BASE_URL: 'http://10.0.2.2:3000' // This is the default for Android emulators
 };
